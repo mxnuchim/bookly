07606f89afcdd73cee1d4c869d8440dde8d3f5cc
mxnuchim <manuchimoliver779@gmail.com>
2025-11-12 22:41:42 +0100

    feat: crud starter

--- main.py
+++ main.py
@@ -1,45 +1,30 @@
-from fastapi import FastAPI, Header
-from typing import Optional
-from pydantic import BaseModel
+from books_data import books
+from fastapi import FastAPI
+from schemas import Book
+from typing import List
+
 
 app = FastAPI()
 
-@app.get('/')
-def read_root():
-    return {"message": "Server s live and running"}
+@app.get('/books', response_model=List[Book])
+async def get_all_books() -> dict:
+    return books
 
-@app.get('/greet')
-# Each path parameter in the handler function will be expected as a query as long as you do not define it in the url
-# In this case, name will be passed as a query and validation is handled by pydantic -> FastAPI relies on pydantic for input validation
-async def greet_user(name: Optional[str] = "User", age: int = 0) -> dict:
-    return {"message": f"Howdy {name}!, You are {age} years old", "server_status": f"100%! The server is live and running 🚀🚀"}
+@app.post('/books')
+async def update_book() -> dict:
+    pass
 
-class CreateBookModel(BaseModel):
-    title: str
-    author: str
-    favorite: bool = False
+@app.get('/book/{book_id}')
+async def get_book_by_id(book_id: int) -> dict:
+    pass
 
-@app.post('/create_book')
-async def create_book(book_data: CreateBookModel) -> dict:
-    print(f"Book data received --->> {book_data.model_dump()}")
-    return {
-        "title": book_data.title,
-        "author": book_data.author,
-        "favorite": book_data.favorite
-    }
+@app.patch('/book/{book_id}')
+async def update_book(book_id: int) -> dict:
+    pass
 
-@app.get("/get_headers", status_code=200) # you can specify the status code to return. status_code is optional, but it's a good practice to set it
-async def get_headers(
-    accept: str = Header(None),
-    content_type: str = Header(None),
-    user_agent: str = Header(None),
-    host: str = Header(None)
-):
-    request_headers: dict = {}
-    
-    request_headers["Accept"] = accept
-    request_headers["Content-Type"] = content_type
-    request_headers["User-Agent"] = user_agent
-    request_headers["Host"] = host
+@app.delete('/book/{book_id}')
+async def delete_book(book_id: int) -> dict:
+    pass
 
-    return {"headers": request_headers}
+
+
---
+++ schemas.py
@@ -0,0 +1,10 @@
+from pydantic import BaseModel
+
+class Book(BaseModel):
+    id: int
+    title: str
+    author: str
+    publisher: str
+    published_date: str
+    page_count: int
+    language: str
