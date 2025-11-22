534a91e96c83d0f3c1b02cd30b58105d430b15f4
mxnuchim <manuchimoliver779@gmail.com>
2025-11-22 07:17:57 +0100

    feat: Books CRUD

--- src/books/books_data.py
+++ src/books/books_data.py
@@ -1,4 +1,4 @@
-from schemas import Book
+from src.books.schemas import Book
 from typing import List
 
 books: List[Book] = [
--- src/books/schemas.py
+++ src/books/schemas.py
@@ -8,3 +8,10 @@ class Book(BaseModel):
     published_date: str
     page_count: int
     language: str
+
+class BookUpdate(BaseModel):
+    title: str
+    author: str
+    publisher: str
+    page_count: int
+    language: str
